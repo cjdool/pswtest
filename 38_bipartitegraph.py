de2374b8550a2f362d22b646243d5e5d33d8b7bd
jj <cjdool@naver.com>
2021-02-16 15:14:49 +0900

    38th first making

--- 38_bipartitegraph.py
+++ 38_bipartitegraph.py
@@ -1,36 +1,37 @@
-from collections import deque
+import sys
+
+def checkbipart(graph, num):
+    visited = [False] * num
+    color = [0] * num
+    for i in range(num):
+        if not visited[i]:
+            visited[i] = True
+            color[0] = 1
+            queue = [i]
+
+            while len(queue) != 0:
+                cur = queue.pop(0)
+                for item in graph[cur]:
+                    if not visited[item]:
+                        color[item] = -1 * color[cur]
+                        visited[item] = True
+                        queue.append(item)
+                    else:
+                        if color[item] == color[cur]:
+                            return False
+
+    return True
 
 
-def bfs(graph, root):
-    visited = [root]
-    queue = deque([root])
-
-    while len(queue) != 0:
-        cur = queue.popleft()
-        for item in graph[cur]:
-            if item not in visited:
-                visited.append(item)
-                queue.append(item)
-    return visited
-
-
-def checkbipart(graph):
-
-    return False
-
-K = int(input())
-caselist = []
+K = int(sys.stdin.readline())
 for _ in range(K):
-    V, E = map(int, input().split())
+    V, E = map(int, sys.stdin.readline().split())
     rellist = [[] for _ in range(V)]
     for i in range(E):
-        u, v = map(int, input().split())
+        u, v = map(int, sys.stdin.readline().split())
         rellist[u-1].append(v-1)
         rellist[v-1].append(u-1)
-    caselist.append(rellist)
-
-for case in caselist:
-    if checkbipart(case):
+    if checkbipart(rellist, V):
         print('YES')
     else:
         print('NO')
